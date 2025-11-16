bd1c68dd0c5bd871e5cf5571a4e29e90f7b1971e
Arush Choudhary <arushgzbin@gmail.com>
2025-11-16 12:43:11 +0530

    added the message and chat models

---
+++ src/models/chat.models.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const chatSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "ChatMessage",
+    },
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    admin: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+)
+
+export const Chat = mongoose.model("Chat", chatSchema)
